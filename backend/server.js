3c624130d1152b39e37bee85ddea40fbbb6b410d
malatesh s hulbanni <mshulbanni95@gmail.com>
2026-04-14 10:19:32 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -60,6 +60,17 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
+// For local development
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+// Export for Vercel
+module.exports = app;
